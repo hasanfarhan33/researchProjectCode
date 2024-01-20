368ca7ece758417d358d3d4348642489477c6193
Farhan Hasan <hasanfarhan33@gmail.com>
2024-01-20 23:18:47 +0000

    Implemented OOP

--- carlaMain.py
+++ carlaMain.py
@@ -1,7 +1,9 @@
+import time
 import carla 
 import random 
 import numpy as np
 import cv2 
+import math
 
 actorList = []
 
@@ -10,16 +12,107 @@ models = ["dodge", "audi", "mini", "mustang", "nissan", "jeep"]
 
 IM_WIDTH = 640
 IM_HEIGHT = 480
-
-# Processing image from camera sensor 
-def process_img(image):
-    i = np.array(image.raw_data)
-    i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
-    i3 = i2[:, :, :3]
-    cv2.imshow("", i3)
-    cv2.waitKey(1)
-    return i3/255.0
     
+SHOW_PREVIEW = False
+SECONDS_PER_EPISODE = 10
+
+class CarEnvironment: 
+    SHOW_CAM = SHOW_PREVIEW
+    STEER_AMT = 1.0
+    im_WIDTH = IM_WIDTH 
+    im_HEIGHT = IM_HEIGHT 
+    rear_camera = None 
+    
+    def __init__(self):
+        self.client = carla.Client("localhost", 2000)
+        self.client.set_timeout(50.0)
+        self.world = self.client.get_world() 
+        self.blueprint_library = self.world.get_blueprint_library()
+        self.model3 = blueprint_library.filter("model3")[0]
+        
+    def reset(self):
+        self.collision_history = []
+        self.actor_list = [] 
+        
+        self.transform = random.choice(self.world.get_map().get_spawn_points())
+        self.vehicle = self.world.spawn_actor(self.model3, self.transform)
+        
+        self.actor_list.append(self.vehicle)
+        
+        self.rgb_cam = self.blueprint_library.find("sensor.camera.rgb")
+        self.rgb_cam.set_attribute("image_size_x", f"{self.im_WIDTH}")
+        self.rgb_cam.set_attribute("image_size_y", f"{self.im_HEIGHT}")
+        self.rgb_cam.set_attribute("fov", f"110")
+        
+        transform = carla.Transform(carla.Location(x = -5, z = 2))
+        self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to = self.vehicle)
+        self.actor_list.append(self.sensor)
+        self.sensor.listen(lambda data: self.process_img(data))
+        
+        self.vehicle.apply_control(carla.VehicleControl(throttle = 0.0, brake = 0.0))
+        time.sleep(4)
+        
+        col_sensor = self.blueprint_library.find("sensor.other.collision")
+        self.col_sensor = self.world.spawn_actor(col_sensor, transform, attach_to = self.vehicle)
+        self.actor_list.append(self.col_sensor)
+        self.col_sensor.listen(lambda event: self.collision_data(event))
+        
+        while self.front_camera is None: 
+            time.sleep(0.01)
+            
+        self.episode_start = time.time() 
+        
+        self.vehicle.apply_control(carla.VehicleControl(throttle = 0.0, brake = 0.0))
+        
+        return self.front_camera
+    
+    def collision_data(self, event):
+        self.collision_history.append(event)
+    
+    # Processing image from camera sensor 
+    def process_img(self, image):
+        i = np.array(image.raw_data)
+        i2 = i.reshape((self.im_HEIGHT, self.im_WIDTH, 4))
+        i3 = i2[:, :, :3]
+        
+        if self.SHOW_CAM: 
+            cv2.imshow("", i3)
+            cv2.waitKey(1)
+        
+        self.front_camera = i3
+        
+    def step(self, action):
+        # Go left 
+        if action == 0: 
+            self.vehicle.apply_control(carla.VehicleControl(throttle = 0.2, steer = -1.0 * self.STEER_AMT))
+        # Go straight
+        elif action == 1:
+            self.vehicle.apply_control(carla.VehicleControl(throttle = 0.2, steer = 0))
+        # Turn right
+        elif action == 2:
+            self.vehicle.apply_control(carla.VehicleControl(throttle = 1.0, steer = 1.0 * self.STEER_AMT))
+            
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+        
+        if len(self.collision_history) != 0: 
+            done = True 
+            reward = -200 
+        elif kmh < 50: 
+            done = False 
+            reward = -1 
+        else: 
+            done = False 
+            reward = 1 
+            
+        if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            done = True 
+        
+        return self.front_camera, reward, done, None 
+            
+            
+        
+        
 
 def spawn_traffic(models, maxVehicles):
     manage_traffic()
